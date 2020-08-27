bbd68d2a1c2cd1ce82d5e1e66189036fcbc7a97e
ilizeeth <lizeeth_medina@outlook.com>
2020-08-27 14:06:05 -0500

    AreaTriangulo

---
+++ Areatriangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float lbase,laltura;
+            float area;
+
+            Console.WriteLine("Dame la base"); lbase=float.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la altura"); laltura=float.Parse(Console.ReadLine());
+
+            area=lbase*laltura/2;
+
+            Console.WriteLine($"Un triagulo de base {lbase} y altura {laltura}tiene una area de {area}");
+        }
+
+    }
+}
