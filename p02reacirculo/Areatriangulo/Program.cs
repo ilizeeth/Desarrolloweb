2e15670b7b7d319ed80827b6ff8de7dc278e5c65
ilizeeth <lizeeth_medina@outlook.com>
2020-08-27 14:06:53 -0500

    Circulo

---
+++ p02reacirculo/Areatriangulo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ p02reacirculo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace p02reacirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float radio=0;
+            double area=0;
+
+            Console.Clear(); //borra pantalla
+            Console.WriteLine("Dame el radio del circulo");
+            radio=float.Parse(Console.ReadLine());
+            area=Math.PI*Math.Pow(radio,2);
+
+            Console.WriteLine($"El area es {area}");
+
+
+        }
+    }
+}
