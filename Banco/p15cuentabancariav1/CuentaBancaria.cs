6fcee10c31d35430800c308e0f361490e67dad86
ilizeeth <lizeeth_medina@outlook.com>
2020-09-10 14:03:48 -0500

    p15cuentabancariav1

---
+++ Banco/p15cuentabancariav1/CuentaBancaria.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace p15cuentabancariav1
+{
+   class CuentaBancaria {
+       private double saldo;
+
+       public CuentaBancaria(double saldo) {
+           this.saldo = saldo;
+       }
+       public double Saldo {
+           get { return saldo;}
+       }
+        public void Deposita(double cant) {
+            saldo+=cant;
+        }
+        public bool Retira(double cant) {
+            if(saldo>=cant) {
+                saldo-=cant;
+                return true;
+            } else return false;
+        }
+   } 
+
+}
---
+++ Banco/p15cuentabancariav1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace p15cuentabancariav1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco mibanco = new Banco("La alcancía","Paola Medina");
+            mibanco.AgregarCliente(new Cliente("Juana Gallo"));
+            mibanco.AgregarCliente(new Cliente("Miguel Hidalgo"));
+            mibanco.AgregarCliente(new Cliente("Juan Escutia"));
+            mibanco.AgregarCliente(new Cliente("Pedro Lopez"));
+
+            mibanco.Clientes[0].Cuenta=new CuentaBancaria(100);
+            mibanco.Clientes[1].Cuenta=new CuentaBancaria(600);
+            mibanco.Clientes[2].Cuenta=new CuentaBancaria(10000);
+            mibanco.Clientes[3].Cuenta=new CuentaBancaria(200);
+            mibanco.Clientes[2].Cuenta.Retira(200);
+            mibanco.Clientes[1].Cuenta.Deposita(20);
+
+            Console.WriteLine("Reporte General \n");
+            Console.WriteLine($"Banco: {mibanco.Nombre} \n Propietario de {mibanco.Propietario}\n");
+
+            foreach(Cliente cte in mibanco.Clientes){
+                Console.WriteLine($"El cliente ed nombre: {cte.Nombre}");
+                Console.WriteLine($"Tiene un saldo de: {cte.Cuenta.Saldo}");
+            }     
+        }
+    }
+}
