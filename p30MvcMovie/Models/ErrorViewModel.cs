e823a141c867d8ba6702429335c52a7a0fa81b05
ilizeeth <lizeeth_medina@outlook.com>
2020-10-09 14:04:40 -0500

    Tarea10

---
+++ p30MvcMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace p30MvcMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
